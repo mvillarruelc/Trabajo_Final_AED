a6f9c77b4beb60731aff79c68a4f712854cfaf1d
mvillarruelc <MarcovillarruelC@gmail.com>
2024-10-19 04:21:45 -0500

    Complete Menú
    -Completar Opciones de Menú.
    -Añadir carpeta imágenes.
    -Añadir Fondos.
    -Añadir Iconos al menú (incompleto).
    -Maximizar Ventana.
    -Ajustar tamaño Fondo.

--- Trabajo_Final_AED/src/gui/Inicio.java
+++ Trabajo_Final_AED/src/gui/Inicio.java
@@ -11,6 +11,10 @@ import javax.swing.JMenu;
 import java.awt.GridLayout;
 import javax.swing.JSeparator;
 import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
+import java.awt.BorderLayout;
 
 public class Inicio extends JFrame {
 
@@ -25,6 +29,7 @@ public class Inicio extends JFrame {
 			public void run() {
 				try {
 					Inicio frame = new Inicio();
+					frame.setExtendedState(MAXIMIZED_BOTH);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -37,6 +42,7 @@ public class Inicio extends JFrame {
 	 * Create the frame.
 	 */
 	public Inicio() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Inicio.class.getResource("/Imagenes/FondoCarrito1.jpg")));
 		setTitle("Sistema de Ventas - Inicio");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 750, 500);
@@ -62,14 +68,15 @@ public class Inicio extends JFrame {
 		JMenu mnMantenimiento = new JMenu("Mantenimiento");
 		menuBar.add(mnMantenimiento);
 		
-		JMenuItem mntmClientes = new JMenuItem("Clientes");
-		mnMantenimiento.add(mntmClientes);
+		JMenuItem mntmMClientes = new JMenuItem("M. Clientes");
+		mnMantenimiento.add(mntmMClientes);
 		
 		JSeparator separator_1 = new JSeparator();
 		mnMantenimiento.add(separator_1);
 		
-		JMenuItem mntmProductos = new JMenuItem("Productos");
-		mnMantenimiento.add(mntmProductos);
+		JMenuItem mntmMProductos = new JMenuItem("M. Productos");
+		mntmMProductos.setIcon(new ImageIcon(Inicio.class.getResource("/Imagenes/IconoProducto1.png")));
+		mnMantenimiento.add(mntmMProductos);
 		
 		JLabel lblNewLabel_1 = new JLabel("|");
 		menuBar.add(lblNewLabel_1);
@@ -77,8 +84,9 @@ public class Inicio extends JFrame {
 		JMenu mnVentas = new JMenu("Ventas");
 		menuBar.add(mnVentas);
 		
-		JMenuItem mntmVentas = new JMenuItem("Ventas");
-		mnVentas.add(mntmVentas);
+		JMenuItem mntmVender = new JMenuItem("Vender");
+		mntmVender.setIcon(new ImageIcon(Inicio.class.getResource("/Imagenes/IconoVenta1.png")));
+		mnVentas.add(mntmVender);
 		
 		JLabel lblNewLabel_2 = new JLabel("|");
 		menuBar.add(lblNewLabel_2);
@@ -86,18 +94,32 @@ public class Inicio extends JFrame {
 		JMenu mnAlmacen = new JMenu("Almacén");
 		menuBar.add(mnAlmacen);
 		
-		JMenuItem mntmAlmacen = new JMenuItem("Almacén");
-		mnAlmacen.add(mntmAlmacen);
+		JMenuItem mntmVerAlmacen = new JMenuItem("Ver A.");
+		mntmVerAlmacen.setIcon(new ImageIcon(Inicio.class.getResource("/Imagenes/IconoAlmacen1.png")));
+		mnAlmacen.add(mntmVerAlmacen);
 		
 		JLabel lblNewLabel_3 = new JLabel("|");
 		menuBar.add(lblNewLabel_3);
 		
 		JMenu mnReportes = new JMenu("Reportes");
 		menuBar.add(mnReportes);
+		
+		JMenuItem mntmVerReporte = new JMenuItem("Ver R.");
+		mntmVerReporte.setIcon(new ImageIcon(Inicio.class.getResource("/Imagenes/IconoReporte1.png")));
+		mnReportes.add(mntmVerReporte);
+		
+		JMenuItem mntmCrearReporte = new JMenuItem("Crear R.");
+		mntmCrearReporte.setIcon(new ImageIcon(Inicio.class.getResource("/Imagenes/IconoReporte2.png")));
+		mnReportes.add(mntmCrearReporte);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(1, 0, 0, 0));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblFondo = new JLabel("");
+		lblFondo.setIcon(new ImageIcon(Inicio.class.getResource("/Imagenes/FondoSuperMercado1.jpeg")));
+		
+		contentPane.add(lblFondo);
 		this.setLocationRelativeTo(null);
 		
 	}
